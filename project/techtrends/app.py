679e9cbecff66547f63def77eaca3cc089e4499c
Richard Irving <richard.irving3@dwp.gov.uk>
2022-09-22 07:32:04 +0100

    Dockerfile updates

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,12 +2,15 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+from datetime import datetime
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -22,7 +25,7 @@ def get_post(post_id):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -30,22 +33,25 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      log_msg('None existing article accessed - 404 returned')
       return render_template('404.html'), 404
     else:
+      log_msg('Article "{title}" retrieved'.format(title=post['title']))
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    log_msg('About Us page retrieved')
     return render_template('about.html')
 
-# Define the post creation functionality 
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -60,11 +66,43 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            log_msg('New article "{titled}" added'.format(titled=title))
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+# Define the metrics functionality
+@app.route('/metrics')
+def metrics():
+    try:
+        connection = get_db_connection()
+        connection.cursor()
+        posts = connection.execute('SELECT * FROM posts').fetchall()
+        connection.close()
+        post_num = len(posts)
+        data = {"db_connection_count": connection_count,"post_count": post_num}
+        return data
+    except Exception:
+        return {'result': 'ERROR - No metrics'}, 500
+
+# Define the health check functionality
+@app.route('/healthz')
+def healthz():
+    try:
+        connection = get_db_connection()
+        connection.cursor()
+        connection.execute('SELECT * FROM posts')
+        connection.close()
+        return {'result': 'OK - healthy'}
+    except Exception:
+        return {'result': 'ERROR - unhealthy'}, 500
+
+# Add messages to the Log
+def log_msg(msg):
+    app.logger.info('{time}', '{message}'.format(time=datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), message=msg))
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    # Stream logs to a file, and set the default log level to DEBUG
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    app.run(host='0.0.0.0', port='3111')
